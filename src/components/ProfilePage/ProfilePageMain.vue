fb737966adaccd0e27926f90d9ff976aa5c29d65
SWYOD <67761337+SWYOD@users.noreply.github.com>
2025-03-03 11:31:24 +0300

    final frontend revision

--- src/components/ProfilePage/ProfilePageMain.vue
+++ src/components/ProfilePage/ProfilePageMain.vue
@@ -1,7 +1,13 @@
 <script setup>
 import {onMounted, ref} from "vue";
 import axios from "axios";
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+
+const goToSchedule = () => {
+  router.push('/schedule');
+};
 const props = defineProps([
     'name',
     'surname',
@@ -14,6 +20,18 @@ const surname = ref('');
 const email = ref('');
 const avatar = ref('');
 const cover = ref('');
+const bDate = ref('');
+const groupname = ref('');
+
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}.${month}.${year}`;
+};
+
 
 
 onMounted(async () => {
@@ -27,6 +45,8 @@ onMounted(async () => {
     email.value = response.data.email;
     avatar.value = response.data.avatar;
     cover.value = response.data.cover;
+    bDate.value = formatDate(response.data.bDate);
+    groupname.value = response.data.group.name;
 
   } catch (error) {
     console.log(error)
@@ -47,16 +67,36 @@ onMounted(async () => {
     <div class="profile-page-content">
       <div class="profile-page-content-username">
         {{username}}
+        <button class="schedule-button" @click="goToSchedule"> К расписанию</button>
+
       </div>
       <div class="profile-page-content-data">
-        <div class="profile-page-content-data-name">
-          {{name}}
+        <div class="profile-page-content-data-personal-data">
+          <h1>Личные данные</h1>
+          <div class="profile-page-content-data-personal-data-content">
+            <h2>Имя</h2>
+            <p>{{name}}</p>
+            <h2>Фамилия</h2>
+            <p>{{surname}}</p>
+            <h2>Дата рождения</h2>
+            <p>{{bDate}}</p>
+          </div>
         </div>
-        <div class="profile-page-content-data-surname">
-          {{surname}}
+        <div class="profile-page-content-data-registration-data">
+          <h1>Регистрационные данные</h1>
+          <div class="profile-page-content-data-registration-data-content">
+            <h2>Имя пользователя</h2>
+            <p>{{username}}</p>
+            <h2>Почта</h2>
+            <p>{{email}}</p>
+          </div>
         </div>
-        <div class="profile-page-content-data-email">
-          {{email}}
+        <div class="profile-page-content-data-student-data">
+          <h1>Данные студента</h1>
+          <div class="profile-page-content-data-student-data-content">
+            <h2>Группа</h2>
+            <p>{{groupname}}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -100,22 +140,61 @@ onMounted(async () => {
   gap: 20px;
 }
 .profile-page-content-username {
+  position: relative;
   height: fit-content;
   font-weight: 400;
   font-size: 96px;
   line-height: 30px;
   color: #000000;
-
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 .profile-page-content-data {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 20px;
 }
+.profile-page-content-data-personal-data{
+  display: grid;
+  grid-template-rows: 120px 1fr;
+}
+.profile-page-content-data-registration-data{
+  display: grid;
+  grid-template-rows: 120px 1fr;
+}
+.profile-page-content-data-student-data{
+  display: grid;
+  grid-template-rows: 120px 1fr;
+}
 .avatar{
   width: 100%;
   height: auto;
 }
-
-
+h1{
+  margin: 30px 0px;
+  font-weight: 400;
+  font-size: 36px;
+}
+h2{
+  font-weight: 400;
+  font-size: 24px;
+}
+p{
+  margin-top: 0;
+  font-weight: 400;
+  font-size: 34px;
+}
+.schedule-button{
+  background-color: #7EB8D380;
+  color: #1E465D;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 18px;
+  cursor: pointer;
+  font-size: 32px;
+}
+.schedule-button:hover{
+  background-color: #7EB8D3C0;
+}
 </style>
